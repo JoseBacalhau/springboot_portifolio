bff3468f95115aadfdabe7c4fdd93f30d4b7574c
Vitor <jvmbacalhau@gmail.com>
2023-05-08 23:26:01 -0300

    Atividade spring

---
+++ springboot_portifolio/src/main/java/com/example/springboot_portifolio/SpringbootPortifolioApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_portifolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPortifolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPortifolioApplication.class, args);
+	}
+
+}
---
+++ springboot_portifolio/src/test/java/com/example/springboot_portifolio/SpringbootPortifolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_portifolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPortifolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
